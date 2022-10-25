28bf8735cf5b630a1c6f041228aa6233b9a77339
DiegolPl <diegopdl1992@gmail.com>
2022-10-25 00:02:25 -0300

    Fix a ruta de CrossOrigin

--- src/main/java/com/proyectoAP/curriculum/controller/LoginController.java
+++ src/main/java/com/proyectoAP/curriculum/controller/LoginController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins="http://localhost:4200")
+@CrossOrigin(origins={"http://localhost:4200"})
 @RequestMapping("/login")
 public class LoginController {
     
