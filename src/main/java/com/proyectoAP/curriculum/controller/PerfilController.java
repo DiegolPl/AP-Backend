0aea34db4e15efe0c06cc47765575fec366c8d3f
DiegolPl <diegopdl1992@gmail.com>
2022-10-29 19:03:15 -0300

    Correccion de endpoint para editar perfil

--- src/main/java/com/proyectoAP/curriculum/controller/PerfilController.java
+++ src/main/java/com/proyectoAP/curriculum/controller/PerfilController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -33,50 +34,55 @@ public class PerfilController {
     }
     
     @PutMapping("/editar")
-    public void editPerfil(@RequestParam("id") String idperfil, @RequestParam("nombre") String nombre, @RequestParam("titulo") String titulo, @RequestParam("sobreMi") String sobreMi, @RequestParam("archivo") MultipartFile imagen){
-        
-        //System.out.println("Entro?");
-        
-        Long idPerfil = Long.parseLong(idperfil);
-        
-        Perfil perfil = perfServ.getData(idPerfil);
-        perfil.setId(idPerfil);
-        perfil.setNombre(nombre);
-        perfil.setTitulo(titulo);
-        perfil.setSobreMi(sobreMi);
-        
-        if(imagen == null){
-            perfServ.editPerfil(perfil);
-        }else{
-            Archivo newImg = new Archivo();
-            if(!imagen.isEmpty()){
-                //Obtenemos la ruta relativa donde se va a almacenar la imagen
-                Path directorioImagenes = Paths.get("src//main//resources//static//images/perfil");
-                //Obtenemos la ruta absoluta
-                String rutaAbsoluta = directorioImagenes.toFile().getAbsolutePath();
-                //Leemos los bytes de la imagen
-                try {
-                    //Obtenemos la imagen en bytes y los guardamos en una variable
-                    byte[] bytesImg = imagen.getBytes();
-                    //Establecemos la ruta donde se guardara el archivo
-                    //Path rutaCompleta = Paths.get(rutaAbsoluta + "//" + imagen.getOriginalFilename());
-                    Path rutaCompleta = Paths.get(rutaAbsoluta + "//perfil.jpg");
-                    //Escribimos el archivo
-                    Files.write(rutaCompleta, bytesImg);
-
-                    //newImg.setNombre(imagen.getOriginalFilename());
-                    newImg.setUrl(rutaCompleta.toString());
-
-                    perfil.setUrlImg(rutaCompleta.toString());
-
-                } catch(IOException e){
-                    e.printStackTrace();
-                }
-            }
-            perfServ.editPerfil(perfil);
-        }
-        
+    public void editPerfil(@RequestBody Perfil perfil){
+        perfServ.editPerfil(perfil);
     }
+    
+//    @PutMapping("/editar")
+//    public void editPerfil(@RequestParam("id") String idperfil, @RequestParam("nombre") String nombre, @RequestParam("titulo") String titulo, @RequestParam("sobreMi") String sobreMi, @RequestParam("archivo") MultipartFile imagen){
+//        
+//        //System.out.println("Entro?");
+//        
+//        Long idPerfil = Long.parseLong(idperfil);
+//        
+//        Perfil perfil = perfServ.getData(idPerfil);
+//        perfil.setId(idPerfil);
+//        perfil.setNombre(nombre);
+//        perfil.setTitulo(titulo);
+//        perfil.setSobreMi(sobreMi);
+//        
+//        if(imagen == null){
+//            perfServ.editPerfil(perfil);
+//        }else{
+//            Archivo newImg = new Archivo();
+//            if(!imagen.isEmpty()){
+//                //Obtenemos la ruta relativa donde se va a almacenar la imagen
+//                Path directorioImagenes = Paths.get("src//main//resources//static//images/perfil");
+//                //Obtenemos la ruta absoluta
+//                String rutaAbsoluta = directorioImagenes.toFile().getAbsolutePath();
+//                //Leemos los bytes de la imagen
+//                try {
+//                    //Obtenemos la imagen en bytes y los guardamos en una variable
+//                    byte[] bytesImg = imagen.getBytes();
+//                    //Establecemos la ruta donde se guardara el archivo
+//                    //Path rutaCompleta = Paths.get(rutaAbsoluta + "//" + imagen.getOriginalFilename());
+//                    Path rutaCompleta = Paths.get(rutaAbsoluta + "//perfil.jpg");
+//                    //Escribimos el archivo
+//                    Files.write(rutaCompleta, bytesImg);
+//
+//                    //newImg.setNombre(imagen.getOriginalFilename());
+//                    newImg.setUrl(rutaCompleta.toString());
+//
+//                    perfil.setUrlImg(rutaCompleta.toString());
+//
+//                } catch(IOException e){
+//                    e.printStackTrace();
+//                }
+//            }
+//            perfServ.editPerfil(perfil);
+//        }
+//        
+//    }
     
      
 //    @PutMapping("/editar-sin-foto")
