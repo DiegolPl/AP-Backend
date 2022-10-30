9c50c6cecae40b9dfe89bf2bce27ba9d710d948d
DiegolPl <diegopdl1992@gmail.com>
2022-10-29 22:10:22 -0300

    Correccion de endpoint para crear y editar proyecto

--- src/main/java/com/proyectoAP/curriculum/controller/ProyectoController.java
+++ src/main/java/com/proyectoAP/curriculum/controller/ProyectoController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -40,109 +41,119 @@ public class ProyectoController {
     }
     
     @PostMapping("/crear")
-    public void addProyecto(@RequestParam("titulo") String titulo, @RequestParam("fecha") String fecha, @RequestParam("link") String link, @RequestParam("descripcion") String descripcion, @RequestParam("archivo") MultipartFile imagen){
-        
-        Proyecto proyecto = new Proyecto();
-//        proyecto.setId(id);
-        proyecto.setTitulo(titulo);
-        proyecto.setFecha(fecha);
-        proyecto.setLink(link);
-        proyecto.setDescripcion(descripcion);
-        
-        Archivo newImg = new Archivo();
-        
-        if(!imagen.isEmpty()){
-            //Obtenemos la ruta relativa donde se va a almacenar la imagen
-            Path directorioImagenes = Paths.get("src//main//resources//static//images/proyectos");
-            //Obtenemos la ruta absoluta
-            String rutaAbsoluta = directorioImagenes.toFile().getAbsolutePath();
-            //Leemos los bytes de la imagen
-            try {
-                //Obtenemos la imagen en bytes y los guardamos en una variable
-                byte[] bytesImg = imagen.getBytes();
-                //Establecemos la ruta donde se guardara el archivo
-                String tituloSinEspacios = titulo.replace(" ","");
-                Path rutaCompleta = Paths.get(rutaAbsoluta + "//" + tituloSinEspacios + imagen.getOriginalFilename());  
-                //Path rutaCompleta = Paths.get(rutaAbsoluta + "//perfil.jpg");
-                //Escribimos el archivo
-                Files.write(rutaCompleta, bytesImg);
-                
-                //newImg.setNombre(imagen.getOriginalFilename());
-                newImg.setUrl(rutaCompleta.toString());
-                
-                proyecto.setUrl_imagen(rutaCompleta.toString());
-                
-            } catch(IOException e){
-                e.printStackTrace();
-            }
-            
-        }
-        
+    public void addProyecto(@RequestBody Proyecto proyecto){
         proyServ.addProyecto(proyecto);
     }
     
+//    @PostMapping("/crear")
+//    public void addProyecto(@RequestParam("titulo") String titulo, @RequestParam("fecha") String fecha, @RequestParam("link") String link, @RequestParam("descripcion") String descripcion, @RequestParam("archivo") MultipartFile imagen){
+//        
+//        Proyecto proyecto = new Proyecto();
+////        proyecto.setId(id);
+//        proyecto.setTitulo(titulo);
+//        proyecto.setFecha(fecha);
+//        proyecto.setLink(link);
+//        proyecto.setDescripcion(descripcion);
+//        
+//        Archivo newImg = new Archivo();
+//        
+//        if(!imagen.isEmpty()){
+//            //Obtenemos la ruta relativa donde se va a almacenar la imagen
+//            Path directorioImagenes = Paths.get("src//main//resources//static//images/proyectos");
+//            //Obtenemos la ruta absoluta
+//            String rutaAbsoluta = directorioImagenes.toFile().getAbsolutePath();
+//            //Leemos los bytes de la imagen
+//            try {
+//                //Obtenemos la imagen en bytes y los guardamos en una variable
+//                byte[] bytesImg = imagen.getBytes();
+//                //Establecemos la ruta donde se guardara el archivo
+//                String tituloSinEspacios = titulo.replace(" ","");
+//                Path rutaCompleta = Paths.get(rutaAbsoluta + "//" + tituloSinEspacios + imagen.getOriginalFilename());  
+//                //Path rutaCompleta = Paths.get(rutaAbsoluta + "//perfil.jpg");
+//                //Escribimos el archivo
+//                Files.write(rutaCompleta, bytesImg);
+//                
+//                //newImg.setNombre(imagen.getOriginalFilename());
+//                newImg.setUrl(rutaCompleta.toString());
+//                
+//                proyecto.setUrl_imagen(rutaCompleta.toString());
+//                
+//            } catch(IOException e){
+//                e.printStackTrace();
+//            }
+//            
+//        }
+//        
+//        proyServ.addProyecto(proyecto);
+//    }
+    
     @PutMapping("/editar")
-    public void editProyecto(@RequestParam("id") String id, @RequestParam("titulo") String titulo, @RequestParam("fecha") String fecha, @RequestParam("link") String link, @RequestParam("descripcion") String descripcion, @RequestParam("archivo") MultipartFile imagen){
-        
-        Long idProy = Long.parseLong(id);
-        
-        Proyecto proyecto = new Proyecto();
-        proyecto.setId(idProy);
-        proyecto.setTitulo(titulo);
-        proyecto.setFecha(fecha);
-        proyecto.setLink(link);
-        proyecto.setDescripcion(descripcion);
-        
-        Archivo newImg = new Archivo();
-        
-        if(!imagen.isEmpty()){
-            //Obtenemos la ruta relativa donde se va a almacenar la imagen
-            Path directorioImagenes = Paths.get("src//main//resources//static//images/proyectos");
-            //Obtenemos la ruta absoluta
-            String rutaAbsoluta = directorioImagenes.toFile().getAbsolutePath();
-            //Leemos los bytes de la imagen
-            try {
-                //Obtenemos la imagen en bytes y los guardamos en una variable
-                byte[] bytesImg = imagen.getBytes();
-                //Establecemos la ruta donde se guardara el archivo
-                Path rutaCompleta = Paths.get(rutaAbsoluta + "//" + imagen.getOriginalFilename());  
-                //Path rutaCompleta = Paths.get(rutaAbsoluta + "//perfil.jpg");
-                //Escribimos el archivo
-                Files.write(rutaCompleta, bytesImg);
-                
-                //newImg.setNombre(imagen.getOriginalFilename());
-                newImg.setUrl(rutaCompleta.toString());
-                
-                proyecto.setUrl_imagen(rutaCompleta.toString());
-                
-            } catch(IOException e){
-                e.printStackTrace();
-            }
-            
-        }
-        
-        
+    public void editProyecto(@RequestBody Proyecto proyecto){
         proyServ.editProyecto(proyecto);
     }
+    
+//    @PutMapping("/editar")
+//    public void editProyecto(@RequestParam("id") String id, @RequestParam("titulo") String titulo, @RequestParam("fecha") String fecha, @RequestParam("link") String link, @RequestParam("descripcion") String descripcion, @RequestParam("archivo") MultipartFile imagen){
+//        
+//        Long idProy = Long.parseLong(id);
+//        
+//        Proyecto proyecto = new Proyecto();
+//        proyecto.setId(idProy);
+//        proyecto.setTitulo(titulo);
+//        proyecto.setFecha(fecha);
+//        proyecto.setLink(link);
+//        proyecto.setDescripcion(descripcion);
+//        
+//        Archivo newImg = new Archivo();
+//        
+//        if(!imagen.isEmpty()){
+//            //Obtenemos la ruta relativa donde se va a almacenar la imagen
+//            Path directorioImagenes = Paths.get("src//main//resources//static//images/proyectos");
+//            //Obtenemos la ruta absoluta
+//            String rutaAbsoluta = directorioImagenes.toFile().getAbsolutePath();
+//            //Leemos los bytes de la imagen
+//            try {
+//                //Obtenemos la imagen en bytes y los guardamos en una variable
+//                byte[] bytesImg = imagen.getBytes();
+//                //Establecemos la ruta donde se guardara el archivo
+//                Path rutaCompleta = Paths.get(rutaAbsoluta + "//" + imagen.getOriginalFilename());  
+//                //Path rutaCompleta = Paths.get(rutaAbsoluta + "//perfil.jpg");
+//                //Escribimos el archivo
+//                Files.write(rutaCompleta, bytesImg);
+//                
+//                //newImg.setNombre(imagen.getOriginalFilename());
+//                newImg.setUrl(rutaCompleta.toString());
+//                
+//                proyecto.setUrl_imagen(rutaCompleta.toString());
+//                
+//            } catch(IOException e){
+//                e.printStackTrace();
+//            }
+//            
+//        }
+//        
+//        
+//        proyServ.editProyecto(proyecto);
+//    }
     
     @DeleteMapping("/delete/{id}")
     public void deleteExperiencia(@PathVariable Long id){
         
-        Proyecto proyecto = proyServ.getProyecto(id);
-        
-        String urlImagen = proyecto.getUrl_imagen();
-        
-        Path path = Paths.get(urlImagen);
-        try {
-            boolean result = Files.deleteIfExists(path);
-            if (result) {
-                System.out.println("File is deleted!");
-            } else {
-                System.out.println("Sorry, could not delete the file.");
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+//        Proyecto proyecto = proyServ.getProyecto(id);
+//        
+//        String urlImagen = proyecto.getUrl_imagen();
+//        
+//        Path path = Paths.get(urlImagen);
+//        try {
+//            boolean result = Files.deleteIfExists(path);
+//            if (result) {
+//                System.out.println("File is deleted!");
+//            } else {
+//                System.out.println("Sorry, could not delete the file.");
+//            }
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
         
         proyServ.deleteProyecto(id);
     }
